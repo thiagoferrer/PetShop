7b23141ebd4961bd28d8c452539a7971b6992c7a
thiagoferrer <thi-ferrer@live.com>
2025-08-11 16:20:33 -0300

    README.MD

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public final static Scanner scanner = new Scanner(System.in);
+
+    private final static PetMachine petMachine = new PetMachine();
+
+    public static void main(String[] args) {
+        scanner.useDelimiter("\\n");
+        var option = -1;
+
+        do {
+            System.out.println("===Escolha das opções===");
+            System.out.println("1 - Dar banho no pet");
+            System.out.println("2 - Abastecer a máquina com água");
+            System.out.println("3 - Abastecer a máquina com shampoo");
+            System.out.println("4 - Verificar água da máquina");
+            System.out.println("5 - Verificar shampoo da máquina");
+            System.out.println("6 - Verificar se tem pet no banho");
+            System.out.println("7 - Colocar pet na máquina");
+            System.out.println("8 - Retirar pet da máquina");
+            System.out.println("9 - Limpar a máquina");
+            System.out.println("0 - sair");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1 -> petMachine.takeAShower();
+                case 2 -> setWater();
+                case 3 -> setShampoo();
+                case 4 -> verifyWater();
+                case 5 -> verifyShampoo();
+                case 6 -> checkIfHasPetInMachine();
+                case 7 -> setPetInPetMachine();
+                case 8 -> petMachine.removePet();
+                case 9 -> petMachine.wash();
+                case 0 -> System.exit(0);
+                default -> System.out.println("Opção inválida");
+            }
+
+        } while (true);
+    }
+
+    private static void setWater() {
+        System.out.println("Tentando colocar água na máquina");
+        petMachine.addWater();
+    }
+
+    private static void setShampoo() {
+        System.out.println("Tentando colocar shampoo na máquina");
+        petMachine.addShampoo();
+    }
+
+    private static void verifyWater() {
+        var amout = petMachine.getWater();
+        System.out.println("A máquina está no momento com " + amout + " litro(s) de água");
+    }
+
+    private static void verifyShampoo() {
+        var amout = petMachine.getShampoo();
+        System.out.println("A máquina está no momento com " + amout + " litro(s) de shampoo");
+    }
+
+    private static void checkIfHasPetInMachine() {
+        var hasPet = petMachine.hasPet();
+        System.out.println(hasPet ? "Tem pet na máquina " : "Não tem pet na máquina");
+    }
+
+    public static void setPetInPetMachine() {
+        var name = "";
+        while (name == null || name.isEmpty()) {
+            System.out.println("Informe no nome do pet: ");
+            name = scanner.next();
         }
+        var pet = new Pet(name);
+        petMachine.setPet(pet);
     }
 }
--- src/Pet.java
+++ src/Pet.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Pet {
+    private final String name;
+    private boolean clean;
+
+    public Pet(String name) {
+        this.name = name;
+        this.clean = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isClean() {
+        return clean;
+    }
+
+    public void setClean(boolean clean) {
+        this.clean = clean;
+    }
 }
